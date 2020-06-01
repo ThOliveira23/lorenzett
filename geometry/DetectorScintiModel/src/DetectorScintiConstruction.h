8e58b6dcb430bb8a1ee975f371c6d4c06d2f8503
[Thales Menezes de Oliveira] <[thales.menezes.de.oliveira@gmail.com]>
2020-06-01 11:49:38 -0300

    DetectorScintiConstruction.h

--- geometry/DetectorScintiModel/src/DetectorScintiConstruction.h
+++ geometry/DetectorScintiModel/src/DetectorScintiConstruction.h
@@ -1,4 +1,61 @@
 #ifndef DetectorScintiConstruction_h
 #define DetectorScintiConstruction_h
 
+#include "GaugiKernel/MsgStream.h"
+#include "G4VUserDetectorConstruction.hh"
+#include "G4Material.hh"
+#include "G4ThreeVector.hh"
+#include "G4Region.hh"
+#include "globals.hh"
+
+// Class definitions
+// The physical volume can be translated or rotated and the logical volume carries the volume itself
+class G4PhysicalVolume;
+class G4GlobalMagFieldMessenger;
+
+// Derive your own concrete class from the G4VUserDetectorConstruction abstract base class
+class DetectorScintiConstruction : public G4VUserDetectorConstruction, public MsgService
+{
+// The method construct()
+// Construct all necessary material
+// Define shapes/solids required to describe the geometry
+// Construct and place volumes of your detector geometry
+
+  public:
+    DetectorScintiConstruction(std::string); // constructor
+    virtual ~DetectorScintiConstruction();   // destructor
+    virtual G4PhysicalVolume* Construct();
+    virtual void ConstructSDandField();
+    
+    // Get methods
+    const G4PhysicalVolume* GetAbsorberPV() const;
+    const G4PhysicalVolume* GetGapPV();
+    
+  private:
+    
+    // Methods
+    void DefineMaterials();
+    
+    G4PhysicalVolume* DefineVolumes();
+    
+    // Creating the regions of the detector
+    void CreateBarrel( G4LogicalVolume *worldLV,
+                       std::string name,
+                       G4Material *defaultMaterial,
+                       G4Material *absorberMaterial,
+                       G4Material *gapMaterial,
+                       int nofLayers,
+                       double absoThickness,
+                       double gapThickness,
+                       double calorRmin,
+                       double calorZ,
+                       G4ThreeVector center_pos,
+                       G4Region* region);
+    
+    static G4ThreadLocal G4GlobalMagFieldMessenger* m_magFieldMessenger; // Magnetic Field Messenger
+    bool m_checkOverlaps; // option to activate checking of volumes overlaps
+    
+};
+
+#endif
 
