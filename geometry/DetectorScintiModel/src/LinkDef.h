64ba61b5649ed824ff7e9d257ad684b1bde3e6e9
[Thales Menezes de Oliveira] <[thales.menezes.de.oliveira@gmail.com]>
2020-06-01 11:20:09 -0300

    LinkDef

--- geometry/DetectorScintiModel/src/LinkDef.h
+++ geometry/DetectorScintiModel/src/LinkDef.h
@@ -1,2 +1,13 @@
 
 #include "src/DetectorScintiConstruction.h"
+
+#ifdef __CINT__
+
+#pragma link of all globals;
+#pragma link of all classes;
+#pragma link of all functions;
+#pragma link of C++ nestedclass;
+
+#pragma link C++ class DetectorScintiConstruction++;
+
+#endif
