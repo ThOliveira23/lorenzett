e6c3cb44661fe972ced251dedea4d1779eea9f1d
[Thales Menezes de Oliveira] <[thales.menezes.de.oliveira@gmail.com]>
2020-06-10 16:01:42 -0300

    .sh

---
+++ reco.sh
@@ -0,0 +1,3 @@
+#reco_trf.py -i def_zee.root --outputLevel 6 -nt 2 -o def_reco_zee.root --cal ATLAS >> ATLAS.log
+#reco_trf.py -i def_zee.root --outputLevel 6 -nt 2 -o def_reco_zee.root --cal Generic >> Generic.log
+reco_trf.py -i def_zee.root --outputLevel 6 -nt 2 -o def_reco_zee.root --cal Scintillator >> Scintillator.log
